44532a46701d08f329c6f1eed3c0a48f68704bba
Estéfano Quiriconi <estefanoquiriconi@gmail.com>
2026-03-12 18:31:59 -0300

    feat(frontend): replace home route with task CRUD routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import TasksView from '@/views/TasksView.vue'
+import TaskFormView from '@/views/TaskFormView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
+    { path: '/', name: 'tasks', component: TasksView },
+    { path: '/tasks/create', name: 'task-create', component: TaskFormView },
+    { path: '/tasks/:id/edit', name: 'task-edit', component: TaskFormView },
   ],
 })
 
