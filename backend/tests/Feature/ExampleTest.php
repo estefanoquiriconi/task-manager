97dd54f0e9aefcfa839275d91b105422b197d6be
Estéfano Quiriconi <estefanoquiriconi@gmail.com>
2026-03-21 19:27:06 -0300

    refactor: remove welcome view and redirect root to API documentation

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('the application returns a successful response', function () {
+test('the application redirects root to the api docs', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertRedirect('/docs/api');
 });
